1f47a419c50a8c1ec6b40c683db08d4dfbfd6224
LJY981008 <88678179+LJY981008@users.noreply.github.com>
2023-01-04 08:59:30 +0900

    서버를 통한 오브젝트 이동 이벤트

---
+++ Assets/Scripts/Packet.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Packet
+{
+    public enum ePACKETTYPE
+    {
+        NONE,
+        PEERINFO = 1000,
+        CHARSELECT,
+        CHARMOVE
+    }
+    public struct PEERINFO
+    {
+        public ePACKETTYPE ePacketType;
+        public int uid;
+        public string name;
+        public short charType;
+    }
+    public struct CHARMOVE
+    {
+        public ePACKETTYPE ePacketType;
+        public int uid;
+        public float xPos;
+        public float yPos;
+        public float zPos;
+    }
+    
+}
